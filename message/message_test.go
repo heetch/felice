a61fee831b65c0e5af08d46900f2dba07cd915b0
Geoffrey J. Teale <tealeg@gmail.com>
2018-10-08 15:49:17 +0200

    Flesh-out test for the minimum possible Message

--- message/message_test.go
+++ message/message_test.go
@@ -13,4 +13,12 @@ func TestNew(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, msg)
 
+	require.NotZero(t, msg.ID)
+	require.Equal(t, "test", msg.Topic)
+	require.Len(t, msg.Headers, 2)
+
+	require.Equal(t, msg.ID, msg.Headers["Message-Id"])
+	require.NotZero(t, msg.Headers["Produced-At"])
+	// "null" is the JSON representation of nil
+	require.Equal(t, []byte("null"), msg.Body)
 }
