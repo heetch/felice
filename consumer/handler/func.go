e0f7b08386609fd153a11d636484320f3f9aec01
Geoffrey J. Teale <tealeg@gmail.com>
2018-10-10 17:03:44 +0200

    Drop HandlerFunc function and rename HandleMessageFn to HandlerFunc

--- consumer/handler/func.go
+++ consumer/handler/func.go
@@ -4,17 +4,11 @@ import (
 	"github.com/heetch/felice/message"
 )
 
-// A HandleMessageFn is a function type that can be passed to
-// HandlerFunc.  Its signature mimics the interface of the
-// HandleMessage function in the Handler interface.
-type HandleMessageFn func(*message.Message) error
+// A HandlerFunc is a function type that mimics the interface of the
+// HandleMessage function in the Handler interface.  If you cast a
+// function to this type, it will comply with the Handler interface.
+type HandlerFunc func(*message.Message) error
 
-func (h *HandleMessageFn) HandleMessage(msg *message.Message) error {
-	return (*h)(msg)
-}
-
-// HandlerFunc wraps a HandleMessagFn in such a way that it complies
-// with the Handler interface.
-func HandlerFunc(fn HandleMessageFn) Handler {
-	return &fn
+func (h HandlerFunc) HandleMessage(msg *message.Message) error {
+	return h(msg)
 }
