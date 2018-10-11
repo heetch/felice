0bd424afdf2983dc20189d35e78a9644e2df7e89
Geoffrey J. Teale <tealeg@gmail.com>
2018-10-11 11:26:58 +0200

    Test the "Happy case" of Get / Set on a single topic

--- consumer/handler/collection_test.go
+++ consumer/handler/collection_test.go
@@ -1,29 +1,47 @@
 package handler_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/heetch/felice/consumer/handler"
+	"github.com/heetch/felice/message"
 	"github.com/stretchr/testify/require"
 )
+
+type testHandler struct {
+	ID int
+}
+
+func (th *testHandler) HandleMessage(m *message.Message) error {
+	return fmt.Errorf("%d", th.ID)
+}
 
 // The Get and Set functions on the Collection type manage a uniqe set
 // of associations between Topics and Handlers.
 func TestCollectionGetSet(t *testing.T) {
-	type testcase struct {
+	type testCase struct {
 		Topic   string
 		Handler handler.Handler
 		OK      bool
 	}
+
+	th1 := &testHandler{ID: 1}
+
 	setups := []struct {
 		Name     string
 		Handlers map[string]handler.Handler
-		Tests    []testcase
+		Tests    []testCase
 	}{
 		{
 			Name:     "Get from an empty Collection",
 			Handlers: make(map[string]handler.Handler),
-			Tests:    []testcase{{Topic: "Shoe", Handler: nil, OK: false}},
+			Tests:    []testCase{{Topic: "Shoe", Handler: nil, OK: false}},
+		},
+		{
+			Name:     "Set and Get the same Topic's handler",
+			Handlers: map[string]handler.Handler{"Shoe": th1},
+			Tests:    []testCase{{Topic: "Shoe", Handler: th1, OK: true}},
 		},
 	}
 
@@ -37,7 +55,8 @@ func TestCollectionGetSet(t *testing.T) {
 				h, ok := c.Get(tc.Topic)
 				if tc.OK {
 					require.True(t, ok)
-					require.Equal(t, tc.Handler, h)
+					msg := &message.Message{}
+					require.Equal(t, tc.Handler.HandleMessage(msg), h.HandleMessage(msg))
 				} else {
 					require.False(t, tc.OK)
 				}
