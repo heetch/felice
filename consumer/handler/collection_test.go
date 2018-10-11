97923e7bf9d514c54c7daf54df1e2eac5e9fb530
Geoffrey J. Teale <tealeg@gmail.com>
2018-10-11 11:33:19 +0200

    Add test for the case where we overwrite an association.

--- consumer/handler/collection_test.go
+++ consumer/handler/collection_test.go
@@ -27,29 +27,43 @@ func TestCollectionGetSet(t *testing.T) {
 	}
 
 	th1 := &testHandler{ID: 1}
+	th2 := &testHandler{ID: 2}
 
 	setups := []struct {
 		Name     string
-		Handlers map[string]handler.Handler
+		Handlers []map[string]handler.Handler
 		Tests    []testCase
 	}{
 		{
 			Name:     "Get from an empty Collection",
-			Handlers: make(map[string]handler.Handler),
+			Handlers: nil,
 			Tests:    []testCase{{Topic: "Shoe", Handler: nil, OK: false}},
 		},
 		{
 			Name:     "Set and Get the same Topic's handler",
-			Handlers: map[string]handler.Handler{"Shoe": th1},
+			Handlers: []map[string]handler.Handler{{"Shoe": th1}},
 			Tests:    []testCase{{Topic: "Shoe", Handler: th1, OK: true}},
+		},
+		{
+			Name: "Overwite topic association",
+			Handlers: []map[string]handler.Handler{
+				{"Shoe": th1},
+				// The association with th2 should
+				// overwrite the association with th1.
+				{"Shoe": th2},
+			},
+			Tests: []testCase{{Topic: "Shoe", Handler: th2, OK: true}},
 		},
 	}
 
 	for _, s := range setups {
 		t.Run(s.Name, func(t *testing.T) {
 			c := &handler.Collection{}
-			for k, v := range s.Handlers {
-				c.Set(k, v)
+			// We apply iterations of handlers in order
+			for _, hi := range s.Handlers {
+				for k, v := range hi {
+					c.Set(k, v)
+				}
 			}
 			for _, tc := range s.Tests {
 				h, ok := c.Get(tc.Topic)
