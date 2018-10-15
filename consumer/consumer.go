712fc9a15b6c2f00c02eca57386b0dd7a8a1f119
Yasss <yassine@heetch.com>
2018-10-15 15:22:22 +0200

    Add Handle method

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -16,3 +16,25 @@ type Consumer struct {
 	quit          chan struct{}
 	RetryInterval time.Duration
 }
+
+// Handle registers the handler for the given topic.
+// Handle must be called before Serve.
+func (c *Consumer) Handle(topic string, h handler.Handler) {
+	c.setup()
+
+	c.handlers.Set(topic, h)
+}
+
+func (c *Consumer) setup() {
+	if c.handlers == nil {
+		c.handlers = &handler.Collection{}
+	}
+
+	if c.quit == nil {
+		c.quit = make(chan struct{})
+	}
+
+	if c.RetryInterval == 0 {
+		c.RetryInterval = time.Second
+	}
+}
