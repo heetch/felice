027fefe554b1be3a9225d5308bf79e54062c62ea
Yasss <yassine@heetch.com>
2018-10-16 09:52:19 +0200

    Add messages and partitions handling + minor test

--- consumer/consumer_internal_test.go
+++ consumer/consumer_internal_test.go
@@ -3,6 +3,7 @@ package consumer
 import (
 	"testing"
 
+	cluster "github.com/bsm/sarama-cluster"
 	"github.com/stretchr/testify/require"
 
 	"github.com/heetch/felice/message"
@@ -21,4 +22,18 @@ func TestHandle(t *testing.T) {
 	res, ok := c.handlers.Get("topic")
 	require.True(t, ok)
 	require.NotNil(t, res)
+}
+
+// PartitionConsumerMock implements the sarama's PartitionConsumer interface for testing purposes.
+// The sarama library already defines a mock for it but we can't use it because it doesn't allow a partition consumer
+// to be created in such a way that you can push messages to it without creating the expectation that ConsumePartition will be called.
+type PartitionConsumerMock struct{}
+
+func TestConsumerHandlePartitionsOnClosedChannel(t *testing.T) {
+	c := Consumer{}
+	ch := make(chan cluster.PartitionConsumer)
+
+	close(ch)
+	err := c.handlePartitions(ch)
+	require.EqualError(t, err, "partition consumer channel closed")
 }
