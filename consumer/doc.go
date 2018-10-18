ba846f20edeacce3dc825c146f983a0c1b10769d
Geoffrey J. Teale <tealeg@gmail.com>
2018-10-18 11:40:59 +0200

    Add initial doc.go for the, as yet, empty consumer package.

---
+++ consumer/doc.go
@@ -0,0 +1,6 @@
+// This package will eventually contain the Consumer type.  The
+// Consumer is felice's primary entrance point for recieving messages
+// from a Kafka cluster.
+package consumer
+
+// BUG(tealeg): Document this fully when we've implemente the Consumer type.
