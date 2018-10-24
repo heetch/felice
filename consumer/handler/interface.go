645bbee66e6508b05bfc6686b4cd9d13adf10804
Geoffrey J. Teale <tealeg@gmail.com>
2018-10-24 15:26:03 +0200

    Remove double spaces after periods.

--- consumer/handler/interface.go
+++ consumer/handler/interface.go
@@ -2,9 +2,9 @@ package handler
 
 import "github.com/heetch/felice/message"
 
-// Handler is the interface for handling consumed messages.  You can
+// Handler is the interface for handling consumed messages. You can
 // either add a compliant HandleMessage function to some type by hand,
-// or make use of the HandlerFunc.  Note that felice Handlers receive
+// or make use of the HandlerFunc. Note that felice Handlers receive
 // felice Message types.
 type Handler interface {
 	HandleMessage(*message.Message) error
