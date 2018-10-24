e27fab8ae859c47429b3eba230906a6ef1400159
Geoffrey J. Teale <tealeg@gmail.com>
2018-10-24 15:26:03 +0200

    Remove double spaces after periods.

--- message/doc.go
+++ message/doc.go
@@ -1,4 +1,4 @@
-// The message package contains the Message type.  When using
+// The message package contains the Message type. When using
 // the felice Producer, Message will be the type you send.
 // When using the felice Consumer, you will register handlers that
 // receive the Message type.
@@ -10,9 +10,9 @@
 // The value passed as the 2nd argument can be any Go type that can be
 // marshaled with encoding/json.
 //
-// New can also be passed zero, one or many additional Options.  An
+// New can also be passed zero, one or many additional Options. An
 // Option is a function that receives a pointer to the Message and can
-// modify it directly prior to it being returned by New.  Two
+// modify it directly prior to it being returned by New. Two
 // predefined Options exists in felice: Header and Key.
 //
 // For example, if you want to create a Message with a custom header
--- message/option.go
+++ message/option.go
@@ -1,13 +1,13 @@
 package message
 
 // Option is a function type that receives a pointer to a Message and
-// modifies it in place.  Options are intended to customize a message
-// before sending it.  You can do this either by passing them as
+// modifies it in place. Options are intended to customize a message
+// before sending it. You can do this either by passing them as
 // paramters to the New function, or by calling them directly against
 // a Message.
 type Option func(*Message)
 
-// Header is an Option that adds a custom header to the message.  You
+// Header is an Option that adds a custom header to the message. You
 // may pass as many Header options to New as you wish. If multiple
 // Header's are defined for the same key, the value of the last one
 // past to New will be the value that appears on the Message.
@@ -17,7 +17,7 @@ func Header(k, v string) Option {
 	}
 }
 
-// Key is an Option that specifies a key for the message.  You should
+// Key is an Option that specifies a key for the message. You should
 // only pass ths once to the New function, but if you pass it multiple
 // times, the value set by the final one you pass will be what is set
 // on the Message when it is returned by New.
