5e3e253a1f888f019753c768ecaeb1afbce8ae6a
Geoffrey John Teale <tealeg@stinky.fritz.box>
2018-11-01 10:29:37 +0100

    Test that consumer.Handle and consumer.Serve emit logs

--- consumer/consumer_test.go
+++ consumer/consumer_test.go
@@ -2,10 +2,12 @@ package consumer_test
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"regexp"
 	"testing"
 
+	cluster "github.com/bsm/sarama-cluster"
 	"github.com/heetch/felice/common"
 	"github.com/heetch/felice/consumer"
 	"github.com/heetch/felice/consumer/handler"
@@ -14,19 +16,72 @@ import (
 )
 
 const (
-	logRegexPrefix = "\\[Felice\\] [0-9]*/[0-1][0-9]/[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]"
+	logRegexPrefix = "\\[Felice\\] [0-9]*/[0-1][0-9]/[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9] "
 )
+
+type TestLogger struct {
+	buf       *bytes.Buffer
+	oldLogger common.StdLogger
+	t         *testing.T
+}
+
+// NewTestLogger constructs a test logger we can make assertions against
+func NewTestLogger(t *testing.T) *TestLogger {
+	tl := &TestLogger{
+		buf:       bytes.NewBuffer([]byte{}),
+		oldLogger: common.Logger,
+		t:         t,
+	}
+	l := log.New(tl.buf, "[Felice] ", log.LstdFlags)
+	common.Logger = l
+
+	return tl
+}
+
+// TearDown sets the common logger back to its previous state
+func (tl *TestLogger) TearDown() {
+	common.Logger = tl.oldLogger
+}
+
+// Skip will jump over a log line we don't care about.  If there's an
+// error reading from the buffer the test will fail.
+func (tl *TestLogger) SkipLogLine(reason string) {
+	_, err := tl.buf.ReadString('\n')
+	require.NoError(tl.t, err)
+	tl.t.Logf("Skipping log line: %s", reason)
+}
+
+//
+func (tl *TestLogger) LogLineMatches(match string) {
+	content, err := tl.buf.ReadString('\n')
+	require.NoError(tl.t, err)
+	require.Regexp(tl.t, regexp.MustCompile(logRegexPrefix+match), content)
+}
+
+var nilHandler = handler.HandlerFunc(func(m *message.Message) error {
+	return nil
+})
 
 // Consumer.Handle emits log messages
 func TestHandleLogs(t *testing.T) {
-	buf := bytes.NewBuffer([]byte{})
-	l := log.New(buf, "[Felice] ", log.LstdFlags)
-	common.Logger = l
+	tl := NewTestLogger(t)
+	defer tl.TearDown()
 	c := &consumer.Consumer{}
-	c.Handle("foo", handler.HandlerFunc(func(m *message.Message) error {
-		return nil
-	}))
-	require.Regexp(t, regexp.MustCompile(
-		logRegexPrefix+` Registered handler. topic="foo"`), buf.String())
+	c.Handle("foo", nilHandler)
+	tl.LogLineMatches(`Registered handler. topic="foo"`)
+}
 
+// Serve emits logs when it cannot create a new consumer
+func TestServeLogsErrorFromNewConsumer(t *testing.T) {
+	tl := NewTestLogger(t)
+	defer tl.TearDown()
+	c := &consumer.Consumer{}
+	c.NewConsumer = func(addrs []string, groupID string, topics []string, config *cluster.Config) (*cluster.Consumer, error) {
+		return nil, fmt.Errorf("oh noes! it doesn't work!")
+	}
+	c.Handle("foo", nilHandler)
+	err := c.Serve("foo")
+	require.Error(t, err)
+	tl.SkipLogLine("registering handler")
+	tl.LogLineMatches(`failed to create a consumer for topics \[foo\] in consumer group "foo-consumer-group": oh noes! it doesn't work!`)
 }
