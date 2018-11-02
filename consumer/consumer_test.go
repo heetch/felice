dd6f190452aab99d8181307d67b5c80e6342bc70
Geoffrey John Teale <tealeg@gmail.com>
2018-11-02 09:38:21 +0100

    Use common.Testlogger instead of defining locally

--- consumer/consumer_test.go
+++ consumer/consumer_test.go
@@ -1,10 +1,7 @@
 package consumer_test
 
 import (
-	"bytes"
 	"fmt"
-	"log"
-	"regexp"
 	"testing"
 
 	cluster "github.com/bsm/sarama-cluster"
@@ -15,56 +12,13 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const (
-	logRegexPrefix = "\\[Felice\\] [0-9]*/[0-1][0-9]/[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9] "
-)
-
-type TestLogger struct {
-	buf       *bytes.Buffer
-	oldLogger common.StdLogger
-	t         *testing.T
-}
-
-// NewTestLogger constructs a test logger we can make assertions against
-func NewTestLogger(t *testing.T) *TestLogger {
-	tl := &TestLogger{
-		buf:       bytes.NewBuffer([]byte{}),
-		oldLogger: common.Logger,
-		t:         t,
-	}
-	l := log.New(tl.buf, "[Felice] ", log.LstdFlags)
-	common.Logger = l
-
-	return tl
-}
-
-// TearDown sets the common logger back to its previous state
-func (tl *TestLogger) TearDown() {
-	common.Logger = tl.oldLogger
-}
-
-// Skip will jump over a log line we don't care about.  If there's an
-// error reading from the buffer the test will fail.
-func (tl *TestLogger) SkipLogLine(reason string) {
-	_, err := tl.buf.ReadString('\n')
-	require.NoError(tl.t, err)
-	tl.t.Logf("Skipping log line: %s", reason)
-}
-
-//
-func (tl *TestLogger) LogLineMatches(match string) {
-	content, err := tl.buf.ReadString('\n')
-	require.NoError(tl.t, err)
-	require.Regexp(tl.t, regexp.MustCompile(logRegexPrefix+match), content)
-}
-
 var nilHandler = handler.HandlerFunc(func(m *message.Message) error {
 	return nil
 })
 
 // Consumer.Handle emits log messages
 func TestHandleLogs(t *testing.T) {
-	tl := NewTestLogger(t)
+	tl := common.NewTestLogger(t)
 	defer tl.TearDown()
 	c := &consumer.Consumer{}
 	c.Handle("foo", nilHandler)
@@ -73,10 +27,10 @@ func TestHandleLogs(t *testing.T) {
 
 // Serve emits logs when it cannot create a new consumer
 func TestServeLogsErrorFromNewConsumer(t *testing.T) {
-	tl := NewTestLogger(t)
+	tl := common.NewTestLogger(t)
 	defer tl.TearDown()
 	c := &consumer.Consumer{}
-	c.NewConsumer = func(addrs []string, groupID string, topics []string, config *cluster.Config) (*cluster.Consumer, error) {
+	c.NewConsumer = func(addrs []string, groupID string, topics []string, config *cluster.Config) (consumer.ClusterConsumer, error) {
 		return nil, fmt.Errorf("oh noes! it doesn't work!")
 	}
 	c.Handle("foo", nilHandler)
