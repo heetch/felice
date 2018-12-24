9da699c7fbb36aa5181832487a0e111231e3a49a
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2018-12-24 15:35:20 +0100

    Add configurable codecs in producer config

--- producer/config.go
+++ producer/config.go
@@ -1,23 +1,38 @@
 package producer
 
-import "github.com/Shopify/sarama"
+import (
+	"github.com/Shopify/sarama"
+	"github.com/heetch/felice/codec"
+)
 
 // Config is used to configure the Producer.
 type Config struct {
 	sarama.Config
-	// here we will add Felice specific configuration.
+
+	// Codec used to encode the body of the message. Is required.
+	Codec codec.Codec
+
+	// Codec used to decode the message key. Defaults to codec.String.
+	KeyCodec codec.Codec
 }
 
-// NewConfig creates a config with sane defaults.
-func NewConfig(clientID string) Config {
-	config := sarama.NewConfig()
-	config.Version = sarama.V1_0_0_0
-	config.ClientID = clientID
-	config.Producer.RequiredAcks = sarama.WaitForAll // Wait for all in-sync replicas to ack the message
-	config.Producer.Retry.Max = 3                    // Retry up to 3 times to produce the message
-	// required for the SyncProducer, see https://godoc.org/github.com/Shopify/sarama#SyncProducer
-	config.Producer.Return.Successes = true
-	config.Producer.Return.Errors = true
+// NewConfig creates a config with sane defaults. Parameter clientID is directly copied set in Sarama.Config.ClientID
+// and the defaultCodec will be used to encode every message with no specified codec.
+func NewConfig(clientID string, defaultCodec codec.Codec) Config {
+	var c Config
 
-	return Config{*config}
+	// Sarama configuration
+	c.Config = *sarama.NewConfig()
+	c.Config.Version = sarama.V1_0_0_0
+	c.Config.ClientID = clientID
+	c.Config.Producer.RequiredAcks = sarama.WaitForAll // Wait for all in-sync replicas to ack the message
+	c.Config.Producer.Retry.Max = 3                    // Retry up to 3 times to produce the message
+	// required for the SyncProducer, see https://godoc.org/github.com/Shopify/sarama#SyncProducer
+	c.Config.Producer.Return.Successes = true
+	c.Config.Producer.Return.Errors = true
+
+	// Felice configuration
+	c.Codec = defaultCodec
+	c.KeyCodec = codec.String() // defaults to String
+	return c
 }
