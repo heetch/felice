9f7d9c80bbb251fc23a605dc19663f75957dca35
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2018-12-24 14:52:15 +0100

    Add String codec

--- codec/codec.go
+++ codec/codec.go
@@ -1,7 +1,61 @@
 package codec
 
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
 // A Codec can encode and decode values.
 type Codec interface {
 	Encode(v interface{}) ([]byte, error)
 	Decode(data []byte, target interface{}) error
+}
+
+type codecFunc struct {
+	encodeFn func(v interface{}) ([]byte, error)
+	decodeFn func(data []byte, target interface{}) error
+}
+
+func (c *codecFunc) Encode(v interface{}) ([]byte, error) {
+	return c.encodeFn(v)
+}
+
+func (c *codecFunc) Decode(data []byte, target interface{}) error {
+	return c.decodeFn(data, target)
+}
+
+// String encodes and decodes strings or byte slices into themselves.
+// It is useful when passing raw data without touching it.
+// The Encode method takes a byte slice, string, stringer or error and returns a byte slice.
+// The Decode method turns data into v without touching it. v must be a pointer to byte slice or a pointer to string.
+func String() Codec {
+	return &codecFunc{
+		func(v interface{}) ([]byte, error) {
+			switch t := v.(type) {
+			case string:
+				return []byte(t), nil
+			case []byte:
+				return t, nil
+			case fmt.Stringer:
+				return []byte(t.String()), nil
+			case error:
+				return []byte(t.Error()), nil
+			default:
+				return nil, errors.Errorf("%v must be a string, a stringer, an error or a byte slice, got %t instead", v, v)
+			}
+		},
+		func(data []byte, target interface{}) error {
+			switch t := target.(type) {
+			case *string:
+				*t = string(data)
+			case *[]byte:
+				*t = data
+			default:
+				return errors.Errorf("%v must be a pointer to string or to a byte slice, got %t instead", target, target)
+			}
+
+			return nil
+		},
+	}
 }
