112a3f0e6dfe83ab12402da8e81f3b276f9b49ab
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2018-12-26 11:19:58 +0100

    Add encoder

---
+++ codec/encoder.go
@@ -0,0 +1,52 @@
+package codec
+
+import (
+	"github.com/Shopify/sarama"
+)
+
+// IntEncoder creates a sarama.Encoder that encodes using the Int64 codec.
+func IntEncoder(v int) sarama.Encoder {
+	return &encoder{codec: Int64(), v: int64(v)}
+}
+
+// FloatEncoder creates a sarama.Encoder that encodes using the Float64 Codec.
+func FloatEncoder(v float64) sarama.Encoder {
+	return &encoder{codec: Float64(), v: v}
+}
+
+// JSONEncoder creates a sarama.Encoder that encodes using the JSON Codec.
+func JSONEncoder(v interface{}) sarama.Encoder {
+	return &encoder{codec: JSON(), v: v}
+}
+
+// encoder implements the sarama.Encoder interface.
+type encoder struct {
+	codec Codec
+	v     interface{}
+	len   int
+	cache []byte
+	err   error
+}
+
+// Length will usually be called by Sarama before Encode.
+// It calls encode and caches the result before returning the
+// length of the output. If there's an error, it must postpone the error reporting
+// to when Sarama calls the Encode method.
+func (e *encoder) Length() int {
+	e.cache, e.err = e.Encode()
+	return len(e.cache)
+}
+
+// Encode returns the cached result if any, otherwise it encodes
+// v using the registered codec.
+func (e *encoder) Encode() ([]byte, error) {
+	if e.err != nil {
+		return nil, e.err
+	}
+
+	if e.cache != nil {
+		return e.cache, nil
+	}
+
+	return e.codec.Encode(e.v)
+}
