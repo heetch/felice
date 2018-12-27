4c7918a836c896780c7440823427c7a6f8e44e81
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2018-12-27 10:14:32 +0100

    Use HandlerConfig instead of Handler in Collection

--- consumer/handler.go
+++ consumer/handler.go
@@ -36,31 +36,31 @@ func (h HandlerFunc) HandleMessage(msg *Message) error {
 	return h(msg)
 }
 
-// A Collection is set of Handler types, keyed by topic. Only one
-// handler may exists in a Collection per topic, but any number of
-// topics may be contained in the Collection. The handler for a topic
+// A Collection is set of HandlerConfig types, keyed by topic. Only one
+// handler config may exists in a Collection per topic, but any number of
+// topics may be contained in the Collection. The handler config for a topic
 // can be added via Set, and retrieved via Get. It is not currently
 // possible to remove a topic. Additionally the set of all topics with
 // handlers can be returned via the Topics function. These actions are
 // safe to use in concurrent code.
 type Collection struct {
 	sync.RWMutex
-	handlers map[string]Handler
+	handlers map[string]HandlerConfig
 	Logger   *log.Logger
 }
 
-// Get returns the Handler associated with a given topic, and a
+// Get returns the HandlerConfig associated with a given topic, and a
 // Boolean value indicating if the topic was found at all. It is safe
 // to use Get from concurrent code.
-func (h *Collection) Get(topic string) (handler Handler, ok bool) {
+func (h *Collection) Get(topic string) (handlerCfg HandlerConfig, ok bool) {
 	h.RLock()
-	handler, ok = h.handlers[topic]
+	handlerCfg, ok = h.handlers[topic]
 	h.RUnlock()
 	return
 }
 
 // Topics returns a slice of all the topic names for which the
-// Collection contains a Handler. It is safe to use Topics from
+// Collection contains a HandlerConfig. It is safe to use Topics from
 // concurrent code.
 func (h *Collection) Topics() []string {
 	h.RLock()
@@ -75,18 +75,25 @@ func (h *Collection) Topics() []string {
 	return topics
 }
 
-// Set associates the given Handler to the given Topic within the
+// Set associates the given HandlerConfig to the given Topic within the
 // collection. If a Handler was already associated with the Topic,
 // then that association will be lost and replaced by the new one. It
 // is safe to use Set from concurrent code.
-func (h *Collection) Set(topic string, handler Handler) {
+func (h *Collection) Set(topic string, handlerCfg HandlerConfig) {
 	h.Lock()
 	if h.handlers == nil {
-		h.handlers = make(map[string]Handler)
+		h.handlers = make(map[string]HandlerConfig)
 	}
 	if h.Logger != nil {
 		h.Logger.Printf("Registered handler. topic=%q\n", topic)
 	}
-	h.handlers[topic] = handler
+	h.handlers[topic] = handlerCfg
 	h.Unlock()
+}
+
+// HandlerConfig describes a handler and its configuration.
+// It is currently used to store the related message unformatter.
+type HandlerConfig struct {
+	Handler     Handler
+	Unformatter MessageUnformatter
 }
