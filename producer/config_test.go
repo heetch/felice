74de2f9294f7d60d821fa2282d3631e7517d96a9
Enrique J. Hernández <enrique@heetch.com>
2021-02-03 11:37:18 +0100

    Add JVMPartitioner used by default

--- producer/config_test.go
+++ producer/config_test.go
@@ -19,4 +19,39 @@ func TestNewConfig(t *testing.T) {
 	require.True(t, c.Config.Producer.Return.Errors)
 
 	require.Equal(t, f, c.Converter)
+
+	t.Run("JVM Partitioner", func(t *testing.T) {
+		subtests := []struct {
+			In  string
+			Out int32
+		}{
+			// https://github.com/aappleby/smhasher/blob/61a0530f28277f2e850bfc39600ce61d02b518de/src/main.cpp#L73
+			{
+				In:  "Murmur2B",
+				Out: 4,
+			},
+			{
+				In:  "Murmur2C",
+				Out: 5,
+			},
+			// https://github.com/burdiyan/kafkautil/blob/master/partitioner_test.go#L20
+			{
+				In:  "foobar",
+				Out: 6,
+			},
+			{
+				In:  "88d7a76c-48d4-4515-9547-8be944be4594",
+				Out: 8,
+			},
+		}
+
+		partitioner := c.Config.Producer.Partitioner("test-topic")
+		for _, st := range subtests {
+			partitionNumber, err := partitioner.Partition(
+				&sarama.ProducerMessage{Key: sarama.StringEncoder(st.In)},
+				12)
+			require.NoError(t, err)
+			require.Equal(t, st.Out, partitionNumber, "expected to use Murmur2 partitioner as default")
+		}
+	})
 }
